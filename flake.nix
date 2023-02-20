d26b2edddfdaf04353c7420607007d111f22a9b1
uonr <34709816+uonr@users.noreply.github.com>
2023-02-20 21:56:40 +0900

    feat: Pixiv

--- flake.nix
+++ flake.nix
@@ -10,8 +10,31 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python3.withPackages
-          (p: with p; [ python-telegram-bot python-dotenv black tweepy httpx ]);
+        python = pkgs.python3.withPackages (p:
+          with p; [
+            python-telegram-bot
+            python-dotenv
+            black
+            tweepy
+            httpx
+            (buildPythonPackage {
+              pname = "PixivPy3";
+              version = "3.7.2";
+              src = pkgs.fetchFromGitHub {
+                owner = "upbit";
+                repo = "pixivpy";
+                rev = "7b20c3bd158d10238d27135309525946d39bdbe4";
+                hash = "sha256-trCb//nFEhRt7V2BXY90tqmdZ7UWwAbzurtKDd0IA9k=";
+              };
+              doCheck = false;
+              propagatedBuildInputs = with pkgs.python3Packages; [
+                cloudscraper
+                requests
+                requests-toolbelt
+                typing-extensions
+              ];
+            })
+          ]);
       in with pkgs; {
         devShells.default = mkShell {
           buildInputs = [ python you-get ];
---
+++ picker/pixiv.py
@@ -0,0 +1,45 @@
+import os
+import re
+import logging
+from typing import Optional
+from pixivpy3 import *
+
+api = AppPixivAPI()
+refresh_token = os.environ["PIXIV_REFRESH_TOKEN"]
+access_token = os.environ["PIXIV_ACCESS_TOKEN"]
+api.set_auth(access_token, refresh_token)
+
+PIXIV_PATTERN = re.compile("^https://www.pixiv.net/artworks/(\d+)")
+
+
+def get_pixiv_id(url: str) -> Optional[str]:
+    result = PIXIV_PATTERN.match(url)
+    if result is None:
+        return None
+    return result.group(1)
+
+
+def get_url(urls: dict) -> str:
+    url = urls.get("original")
+    if url is not None:
+        return url
+    url = urls.get("large")
+    if url is not None:
+        return url
+    return urls["medium"]
+
+
+def pixiv_get(download_path: str, pixiv_id: str):
+    json_result = api.illust_detail(pixiv_id)
+    illust = json_result.get("illust", None)
+    if illust is None:
+        raise RuntimeError(f"Failed to fetch Pixiv post: {json_result}")
+    print(illust.get("meta_pages", []))
+    for image in illust.get("meta_pages", []):
+        url = get_url(image["image_urls"])
+        logging.info(f"Downloading {url}")
+        api.download(url, path=download_path)
+    else:
+        url = get_url(illust["image_urls"])
+        logging.info(f"Downloading {url}")
+        api.download(url, path=download_path)
