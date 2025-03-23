b86ddceed218e725cae3c963e06023fb2e17b955
uonr <dev@yuru.me>
2025-03-24 04:46:04 +0900

    fix: develop environment

--- flake.nix
+++ flake.nix
@@ -6,29 +6,42 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        pythonPackages = ps: with ps; ([
-          setuptools
-          python-telegram-bot
-          python-dotenv
-          black
-          httpx
-        ] ++ python-telegram-bot.optional-dependencies.job-queue);
+        pythonPackages =
+          ps:
+          with ps;
+          (
+            [
+              setuptools
+              python-telegram-bot
+              python-dotenv
+              black
+              httpx
+            ]
+            ++ python-telegram-bot.optional-dependencies.job-queue
+          );
 
         bot = pkgs.python3Packages.buildPythonApplication {
           pname = "supa-maid-bot";
           version = "0.1.0";
           src = ./.;
           buildInputs = [ pkgs.gallery-dl ];
-          propagatedBuildInputs = (pythonPackages pkgs.python3.pkgs) ++ [ pkgs.gallery-dl];
+          propagatedBuildInputs = (pythonPackages pkgs.python3.pkgs) ++ [ pkgs.gallery-dl ];
           nativeBuildInputs = [ pkgs.pkg-config ];
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
         };
-      in {
+      in
+      {
         packages.default = bot;
 
         apps.default = {
@@ -39,15 +52,12 @@
         devShells.default = pkgs.mkShell {
           buildInputs = [
             (pkgs.python3.withPackages pythonPackages)
-            pkgs.you-get
             pkgs.yt-dlp
             pkgs.gallery-dl
           ];
           nativeBuildInputs = [ pkgs.pkg-config ];
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-          shellHook = ''
-            export PYTHONPATH=${pkgs.python3}/${pkgs.python3.sitePackages}
-          '';
         };
-      });
+      }
+    );
 }
